14aee79531c66bb2ae802f08c7a7588143b24b26
«Kwork» <kwork@yandex.ru>
2022-09-29 19:00:11 +0200

    Add code debug

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) throws IllegalAccessException {
-
-
         Presenter presenter = new Presenter();
         presenter.start();
-
     }
 }
